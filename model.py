25cc5c7726abb605f25203f20785a55566bdda96
Marco Soares <106749775+Sengeki1@users.noreply.github.com>
2024-01-19 11:42:58 -0100

    trained model Linear Regression

--- model.py
+++ model.py
@@ -1,40 +1,40 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
+import copy
+import tensorflow as tf
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('df_model.csv')
-del df['statement_freq']
+df = pd.read_csv('df_for_model.csv', sep=',', low_memory=False)
+df = df.drop_duplicates(subset=['balance_after_trans'])
 
-train, valid, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
-cols = ['account_id', 'loan_id', 'account_district_id', 'account_date_opened', 'amount', 'duration', 'payments'] + ['status']
+train, val, test = np.split(df.sample(frac=1), [int(0.6*len(df)), int(0.8*len(df))])
 
-def scale_dataset(dataframe, oversample=False):
-    x = dataframe[dataframe.columns[:-1]].values # features
-    y = dataframe[dataframe.columns[-1]].values # label
+def get_xy(dataframe, y_label, x_labels=None):
+    dataframe = copy.deepcopy(dataframe)
+    if not x_labels:
+        x = dataframe[[c for c in dataframe.columns if c!=y_label]].values
+    else:
+        if len(x_labels) == 1:
+            x = dataframe[x_labels[0]].values.reshape(-1, 1)
+        else:
+            x = dataframe[x_labels].values
+    
+    y = dataframe[y_label].values.reshape(-1, 1)
+    data = np.hstack((x, y))
 
-    scaler = StandardScaler() # fit and transform
-    x = scaler.fit_transform(x)
+    return data, x , y
 
-    if oversample:
-        ros = RandomOverSampler()
-        x, y = ros.fit_resample(x, y) # takes more of the less class and keep sampling
-        # from there to increase the dataset of that smaller class so they can match 
 
-    data = np.hstack((x, np.reshape(y, (-1, 1))))
+_, x_train, y_train = get_xy(train, 'balance_after_trans', x_labels=['client_id'])
+_, x_train, y_train = get_xy(val, 'balance_after_trans', x_labels=['client_id'])
+_, x_train, y_train = get_xy(test, 'balance_after_trans', x_labels=['client_id'])
 
-    return data, x, y
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
-status = ['A', 'B', 'C', 'D']
-for state in status:
-    z = len(train[train['status'] == state])
-    print(f"Lenght of the class {state} for training: {z}")
+print(regressor.score(x_train, y_train))
 
-train, x_train, y_train = scale_dataset(train, oversample=True)
-# print(len(y_train))
-# print(sum(y_train == 'A'))
-# print(sum(y_train == 'B'))
-# print(sum(y_train == 'C'))
-# print(sum(y_train == 'D'))
-valid, x_train, y_train = scale_dataset(valid, oversample=False)
-test, x_train, y_train = scale_dataset(test, oversample=False)
